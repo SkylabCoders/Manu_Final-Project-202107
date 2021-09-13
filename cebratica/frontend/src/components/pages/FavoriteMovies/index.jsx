613c91fa9bd3cfacf40489bf5c10085f53cec48b
Manuel Barra <manuelbarralazo@gmail.com>
2021-09-13 17:16:09 +0200

    Add styles to listpage

--- cebratica/frontend/src/components/pages/FavoriteMovies/index.jsx
+++ cebratica/frontend/src/components/pages/FavoriteMovies/index.jsx
@@ -6,27 +6,30 @@ import './FavouriteList.scss';
 const FavoriteMovies = () => {
   // eslint-disable-next-line max-len
   const localS = Object.entries(localStorage).map((element) => ({ element: JSON.parse(element[0]), film: JSON.parse(element[1]) }));
+  console.log(localS);
   return (
     <section className="detail">
-      <h1 className="detail__h1-title">Your Favourite List</h1>
-      <h2 className="detail__h2-title">Here you have all favorite movies</h2>
+      <h1 className="detail__h1-title">Here you have all favorite movies</h1>
       {localS.map((e) => (
         <div className="detail__conatiner-card">
           <div className="detail__conatiner-card__detail card-detail">
             <h2 className="card-detail__h2-title">{e.film.id.title}</h2>
             <h3 className="card-detail__h3-title">
               Date:
-              <span className="card-detail__date">{e.film.id.release_date}</span>
             </h3>
-            <p className="card-detail__description-text">
+            <span className="card-detail__date">{e.film.id.release_date}</span>
+            <h3 className="card-detail__description-text">
               Description:
-              <span card-detail__description-text__span>{e.film.id.overview}</span>
-            </p>
+            </h3>
+            <span card-detail__description-text__span>{e.film.id.overview}</span>
           </div>
           <div className="detail__conatiner-img img-detail">
             <img className="img-detail__img" src={`https://image.tmdb.org/t/p/w500${e.film.id?.backdrop_path}`} alt="" />
           </div>
-          <DeleteInFavoriteList movie={e.film.id.original_title} />
+          <div className=".button-detail_container">
+            <DeleteInFavoriteList movie={e.film.id.original_title} />
+          </div>
+
         </div>
       ))}
     </section>
